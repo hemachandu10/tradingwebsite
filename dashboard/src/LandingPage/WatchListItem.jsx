86140824847ae42a7c827da03d2d28b109037b7a
hemachandu10 <bhemachandu10@gmail.com>
2026-06-18 19:40:46 +0530

    Fix import path for watchList.css

--- dashboard/src/LandingPage/WatchListItem.jsx
+++ dashboard/src/LandingPage/WatchListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import './watchlist.css';
+import './watchList.css';
 import BuyForm from './buyForm'; 
 import SellForm from './SellForm'
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
